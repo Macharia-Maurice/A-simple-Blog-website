924e972aa9278678fef6b1e10cef613ea01ac3e7
maurice <maurice.maina@students.jkuat.ac.ke>
2023-12-26 18:24:35 +0300

    changes

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,8 +3,34 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def home(request):
+#     return HttpResponse('<h1>Blog-home</h1>')
+
+# def about(request):
+#     return HttpResponse('<h1>Blog-about</h1')
+
+posts=[
+    {
+        'author':'Maurice',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date-posted':'May 6,2024'
+    },
+    {
+        'author':'Sharlene',
+        'title': 'Blog post 2',
+        'content': '2nd post content',
+        'date-posted':'March 16,2024'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog-home</h1>')
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog-about</h1')
+    return render(request, 'blog/about.html')
