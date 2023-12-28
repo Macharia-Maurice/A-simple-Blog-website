d83d6f879af3a6233b42f437f88a3c5668638f4f
maurice <maurice.maina@students.jkuat.ac.ke>
2023-12-28 21:37:24 +0300

    profile page

---
+++ django_project/users/signals.py
@@ -0,0 +1,16 @@
+# signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Profile
+from django.core.exceptions import ObjectDoesNotExist
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
+
+
+
